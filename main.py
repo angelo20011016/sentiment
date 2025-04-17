7d1abc8cb4f07c45e6ecbc7a34c7bdca206f8f96
angelo20011016 <angelo20011016@gmail.com>
2025-04-17 15:11:09 +0800

    添加根目錄

--- main.py
+++ main.py
@@ -10,3 +10,7 @@ nlp = pipeline("sentiment-analysis", device=-1)  # -1 means using CPU
 async def predict(text: str = Body(...)):  # 使用 Body(...) 指定從 body 中接收
     result = nlp(text)
     return {"prediction": result}
+
+@app.get("/")
+async def read_root():
+    return {"message": "API is running!"}
